0c34f5468b38ddccd35ef536bc41d008943176ed
yujeong-world <kyjii082@gmail.com>
2024-03-12 15:03:54 +0900

    헤더 퍼블리싱 - fixed 메뉴, 호버 컬러 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
 import logo from './logo.svg';
 import './App.css';
+import {SectionsContainer, Section, Header, Footer} from 'react-fullpage';
+import $ from 'jquery';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+  <MainPage/>
+
+
+</>
+
+  );
+
+}
+function MainPage() {
+  let options = {
+    anchors: ['sectionOne', 'sectionTwo', 'sectionThree', 'section4'],
+  };
+
+  return (
+      <div>
+          <Header>
+              <div className="nav">
+                  <a href="#sectionOne" className="opa">Intro</a>
+                  <a href="#sectionTwo">time line</a>
+                  <a href="#sectionThree">project</a>
+                  <a href="#section4">end</a>
+              </div>
+
+              <Footer>
+                  <a href="" className="opa">Dcoumentation</a>
+                  <a href="">Example Source</a>
+                  <a href="">About</a>
+              </Footer>
+          </Header>
+
+          <SectionsContainer {...options}>
+
+              <Section>
+                  <div className="wrapper">
+            <div className="typing-demo">
+              <span>안녕하세요.</span>
+            </div>
+              <div className="typing-demo">
+                  <span>끊임없이 도전하는</span>
+              </div>
+              <div className="typing-demo">
+                  <span>웹개발자 김정호입니다.</span>
+              </div>
+          </div>
+
+                  페이지1
+
+
+              </Section>
+
+              <Section>Page 2</Section>
+
+              <Section>Page 3</Section>
+
+              <Section>Page 4</Section>
+
+
+          </SectionsContainer>
+      </div>
   );
 }
 
